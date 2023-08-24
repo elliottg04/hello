066de439d4f0228535481828117c0bf432f3d3eb
Elliott Gray <elliottdgray@gmail.com>
2023-08-24 09:52:06 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,22 @@
+/**
+ * Author: Elliott Gray
+ *         egray10@unl.edu
+ * Date: 2023/08/22
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+
+  printf("Elliott Gray Cybersecurity\n");
+
+
+  return 0;
+}
+
+
+
